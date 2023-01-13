4b96f196f38ef84fadc8f4044df469609b4e9827
kiotac <technology@kiotac.com>
2023-01-14 00:56:58 +0530

    ok

--- Pressable/Pressable.js
+++ Pressable/Pressable.js
@@ -1,93 +1,91 @@
-import {Box, ScrollView, VStack} from 'native-base';
-import React, {useRef, useState} from 'react';
-import {
-  Button,
-  DrawerLayoutAndroid,
-  Text,
-  StyleSheet,
-  View,
-  Image,
-  TextBase,
-} from 'react-native';
-import HeaderBack from '../components/HeaderBack';
-import SourceCode from '../components/SourceCode';
+import { Badge, Box, Flex, HStack, Pressable, Spacer, Text } from "native-base";
+import React from "react";
 
-const DrawerScreen = () => {
-  const [data, setData] = useState();
-  const drawer = useRef(null);
-  const [drawerPosition, setDrawerPosition] = useState('left');
-  const changeDrawerPosition = () => {
-    if (drawerPosition === 'left') {
-      setDrawerPosition('right');
-    } else {
-      setDrawerPosition('left');
-    }
-  };
-
-  const navigationView = () => (
-    <View style={[styles.container, styles.navigationContainer]}>
-      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
-      <Button
-        title="Close drawer"
-        onPress={() => drawer.current.closeDrawer()}
-      />
-    </View>
-  );
+const PressablePage = () => {
   return (
     <>
-    <HeaderBack/>
-      {/* <ScrollView> */}
-        <DrawerLayoutAndroid
-          ref={drawer}
-          drawerWidth={300}
-          drawerPosition={drawerPosition}
-          renderNavigationView={navigationView}>
-          <View style={styles.container}>
-            <Text style={styles.paragraph}>
-              Swipe from the side or press button below to see it!
-            </Text>
-            <VStack space={5}>
-              <Button
+      <Pressable>
+        {({ isHovered, isFocused, isPressed }) => {
+          return (
+            <Box
+              maxW="96"
+              borderWidth="1"
+              borderColor="coolGray.300"
+              shadow="3"
+              bg={
+                isPressed
+                  ? "coolGray.200"
+                  : isHovered
+                  ? "coolGray.200"
+                  : "coolGray.100"
+              }
+              p="5"
+              rounded="8"
+              style={{
+                transform: [
+                  {
+                    scale: isPressed ? 0.96 : 1,
+                  },
+                ],
+              }}
+            >
+              <HStack alignItems="center">
+                <Badge
+                  colorScheme="darkBlue"
+                  _text={{
+                    color: "white",
+                  }}
+                  variant="solid"
+                  rounded="4"
+                >
+                  Button
+                </Badge>
+                <Spacer />
+              </HStack>
+              <Text
+                color="coolGray.800"
+                mt="3"
+                fontWeight="medium"
+                fontSize="xl"
+              >
+                Pressable
+              </Text>
+              <Text mt="2" fontSize="sm" color="coolGray.700">
+                this is pressable card
+              </Text>
+              <Flex>
+                {isFocused ? (
+                  <Text
+                    mt="2"
+                    fontSize={12}
+                    fontWeight="medium"
+                    textDecorationLine="underline"
+                    color="darkBlue.600"
+                    alignSelf="flex-start"
+                  >
+                    Read More
+                  </Text>
+                ) : (
+                  <Text
+                    mt="2"
+                    fontSize={12}
+                    fontWeight="medium"
+                    color="darkBlue.600"
+                  >
+                    Read More
+                  </Text>
+                )}
+              </Flex>
+              {/* <Button
                 title="Open drawer"
                 onPress={() => drawer.current.openDrawer()}
-              />
-              <Button
-                title="Source Code"
-                onPress={() => {
-                  {
-                    setData('drawer');
-                  }
-                }}
-              />
-            </VStack>
-          </View>
-        </DrawerLayoutAndroid>
-
-        {data === 'drawer' && (
-          <Box shadow={7} bgColor={'#fff'} width={'100%'}>
-          <SourceCode/>
-          
-          </Box>
-        )}
-      {/* </ScrollView> */}
+              /> */}
+            </Box>
+          );
+        }}
+      </Pressable>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
-  },
-  navigationContainer: {
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    padding: 16,
-    fontSize: 15,
-    textAlign: 'center',
-  },
-});
-export default DrawerScreen;
+export default PressablePage;
